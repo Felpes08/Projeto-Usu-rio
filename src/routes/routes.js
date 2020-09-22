722a954b829a30ede4764479f7f0bd81d6a3b0cf
Felps <felpes1208@gmail.com>
2020-09-22 19:57:19 -0300

    Backend com SelectAll e SelectDetail

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,3 +5,12 @@ const router = express.Router();
 router.post('/usuarios', UsuarioController.Insert);
  
 module.exports = router;
+router.post('/usuarios', UsuarioController.Insert);
+router.get('/usuarios', UsuarioController.SelectAll);
+router.get('/usuarios/:id', UsuarioController.SelectDetail);
+router.post('/usuarios', UsuarioController.Insert);
+router.get('/usuarios', UsuarioController.SelectAll);
+router.get('/usuarios/:id', UsuarioController.SelectDetail);
+router.put('/usuarios/:id', UsuarioController.Update);
+router.delete('/usuarios/:id', UsuarioController.Delete);
+ 
